31b760f2c1d21743e236f67b9df163f821c44815
harrietlbs <harrietlbsmith@gmail.com>
2019-02-21 09:08:13 +0100

    first commit

---
+++ js-01/directions.js
@@ -0,0 +1,10 @@
+function shortenDirections(directions) {
+    let horizontal = 0;
+    let vertical = 0;
+    
+    for (let i = 0; < directions.length; i++) {
+        const direction
+    }
+    }
+    
+    module.exports = {shortenDirections};
