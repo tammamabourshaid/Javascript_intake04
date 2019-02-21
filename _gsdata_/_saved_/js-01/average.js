33af1499376a116a4a106a1b5735009599707031
tammamabourshaid <Tar19112004$>
2019-02-21 09:18:17 +0100

    js-01 solutions

---
+++ _gsdata_/_saved_/js-01/average.js
@@ -0,0 +1,4 @@
+function getAverage (array){
+return array.reduce((a, b) => a + b) / array.length;
+}
+module.exports={getAverage};
---
+++ _gsdata_/_saved_/js-01/filter.spec.js
@@ -0,0 +1,17 @@
+const { filter } = require("./filter");
+
+describe("filter", () => {
+  it("should filter an array for number larger than 10", () => {
+    const array = [1, 15, 20, 10, 12, 13];
+    const filterFn = number => number > 10;
+    const filteredArray = [15, 20, 12, 13];
+    expect(filter(array, filterFn)).toEqual(filteredArray);
+  });
+
+  it("should expect the filter function to return only booleans", () => {
+    const array = [1, 15, 20, 10, 12, 13];
+    const filterFn = number => (number > 10 ? 1 : 0);
+    const filteredArray = [];
+    expect(filter(array, filterFn)).toEqual(filteredArray);
+  });
+});
