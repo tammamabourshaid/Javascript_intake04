9d3a9b86a0f70107fb5370e7165da640b5c4f396
tina-fernan <tinaaroom@gmail.com>
2019-02-22 10:44:13 +0100

    solutions

--- js-01/filter.js
+++ js-01/filter.js
@@ -1,16 +1,15 @@
-const filter=(array,filterFn) => {
+const filter = (array, filterFn) => {
 
-    let filteredArray=[];
+    let filteredArray = [];
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
-        if(filterFn(element)===true){
-        filteredArray.push(element)
-    
+        if (filterFn(element) === true) {
+            filteredArray.push(element)
+        }
+
     }
-    
     return filteredArray;
-    }
 }
 
- 
-module.exports={filter};
+
+module.exports = { filter };
---
+++ js-02/palindrome.js
@@ -0,0 +1,14 @@
+const isPalindrome = number => {
+    let numberString=number.toString()
+    .split('').reverse().join('');
+
+    if(number === numberString){
+        return true;
+    }
+
+   return false;
+
+    
+}
+
+module.exports={isPalindrome};
