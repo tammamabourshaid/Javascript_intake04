705349b0da79bee111f827caba87c33d938ad809
AdibZidan <94amsz@gmail.com>
2019-02-23 22:31:49 +0100

    done

--- js-02/calculateBasket.js
+++ js-02/calculateBasket.js
@@ -1,9 +1,20 @@
-const calculateBasket = array => {
-    array.forEach(product => {
-        let price,
-            total = (price * amount);
-        console.log(total);
-    });
+const calculateBasket = list => {
+    const converted = convertIntoArray(list),
+        total = calculateTotal(converted);
+    return total;
+};
+
+const convertIntoArray = list => {
+    const converted = Object.values(list);
+    return converted;
+};
+
+const calculateTotal = list => {
+    const reducer = ((accumulator, currentValue) => {
+        return accumulator + (currentValue.amount * currentValue.price);
+    }, 0),
+    total = list.reduce(reducer);
+    return total;
 };
 
 module.exports = { calculateBasket };
