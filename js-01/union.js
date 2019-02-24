c71074e4b4cd16eea9567b8999f4f733cfae00d9
Marj-11 <marj.hajyahya@gmail.com>
2019-02-24 15:28:03 +0100

    new

--- js-01/union.js
+++ js-01/union.js
@@ -1,14 +1,18 @@
-const getUnion = (array1,array2) => {
 
-let arr = [];
 
+function getUnion(array1, array2) {
+    let resultArray = [];
+
+    for ( let i = 0; i < array1.length; i++ ) {
+        for ( let e = 0; e < array2.length; e++ ) {
+            if (array1[i] === array2[e]) resultArray.push(array1[i]);
+            else if (array1[i] === array2[e * 2]) resultArray.pop(array1[i]);        
     
+    }
+}
+    return resultArray.sort();
+}
 
-
-
- }
-
-    
 
 
 
--- js-02/difference.js
+++ js-02/difference.js
@@ -1,5 +1,19 @@
-const getDifference = (array1, array2) => {
-    
-};
+
+function getDifference(array1, array2) {
+    let resultArray = [];
+
+    array2.filter(function (number) {
+        if (!array1.includes(number)) resultArray.push(number);
+    });
+
+    array1.filter(function (number) {
+        if (!array2.includes(number)) resultArray.push(number);
+    });
+
+
+    return resultArray.sort();
+
+}
+
 
 module.exports = { getDifference };
