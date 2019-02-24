f06e7313cf1bb787c28cdeade3340d1bab462e50
AdibZidan <94amsz@gmail.com>
2019-02-24 12:03:23 +0100

    done

--- js-02/flatten.js
+++ js-02/flatten.js
@@ -1,15 +1,15 @@
 const flatten = array => {
-    // const concatenatedArray = concat(...array);
-    // const reducer = (accumulator, value) => {accumulator.concat(value)};
-    // concatenatedArray.reduce(reducer);
-    // console.log(concatenatedArray);
-    // return concatenatedArray;
-    // const flattenedArray =  [].concat.apply([], array);
-    // console.log(flattenedArray);
-    // return flattenedArray;
-    array.forEach(element => {
-        return element.concat();
-    });
+    return flattenArray(array);
+};
+
+const flattenArray = array => {
+    const flatten = array.reduce(
+        (accumulator, currentValue) => accumulator.concat(isArray(currentValue) ? flattenArray(currentValue) : currentValue), []);
+    return flatten;
+};
+
+const isArray = array => {
+    return Array.isArray(array);
 };
 
 module.exports = { flatten };
