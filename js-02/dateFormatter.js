1bfd586a8e38e7247893f3d327bf269197867ef8
tina-fernan <tinaaroom@gmail.com>
2019-02-24 22:36:03 +0100

    modified

--- js-02/dateFormatter.js
+++ js-02/dateFormatter.js
@@ -1,6 +1,9 @@
-const dateFormatter=() => {
-    let date=new Date();
+const dateFormatter= date => {
     
-return dateFormat(date, "dddd, mmmm dS, yyyy, h:MM");
-}
+        let day = date.getDate();
+        let month = date.getMonth() + 1; //Month from 0 to 11
+        let year = date.getFullYear();
+        return '' + year + '-' + (month<=9 ? '0' + month : month) + '-' + (day <= 9 ? '0' + day : day+ date.getHours()
+         + ":" + date.getMinutes() + "0");
+    }
 module.exporters={dateFormatter};
