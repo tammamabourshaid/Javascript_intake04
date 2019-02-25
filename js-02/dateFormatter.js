15713172853b4a5429b8e72e0972da0173b130a3
AdibZidan <94amsz@gmail.com>
2019-02-25 11:50:51 +0100

    used padStart String method

--- js-02/dateFormatter.js
+++ js-02/dateFormatter.js
@@ -1,36 +1,41 @@
 const dateFormatter = someDate => {
-    const date = new Date(someDate),
-        month = getMonth(date),
-        day = getDay(date),
-        year = date.getFullYear(),
-        hours = date.getHours(),
-        minutes = getMinutes(date),
-        correctDate = getCorrect(year, month, day, hours, minutes);
+  const date = new Date(someDate),
+    month = getMonth(date),
+    day = getDay(date),
+    year = date.getFullYear(),
+    hours = date.getHours(),
+    minutes = getMinutes(date),
+    correctDate = getCorrect(year, month, day, hours, minutes);
 
-    return correctDate;
+  return correctDate;
 };
 
 const getMonth = date => {
-    const month = date.getMonth() + 1,
-        correctMonth = month < 10 ? "0" + month : "" + month;
-    return correctMonth;
+  const month = date.getMonth() + 1,
+    monthString = month.toString(),
+    correctMonth = monthString.padStart(2, "0");
+
+  return correctMonth;
 };
 
 const getDay = date => {
-    const day = date.getDate(),
-        correctDay = day < 10 ? "0" + day : "" + day;
-    return correctDay;
+  const day = date.getDate(),
+    dayString = day.toString(),
+    correctDay = dayString.padStart(2, "0");
+
+  return correctDay;
 };
 
 const getMinutes = date => {
-    const minutes = date.getMinutes(),
-        correctMinutes = minutes < 10 ? "0" + minutes : "" + minutes;
-    return correctMinutes;
+  const minutes = date.getMinutes(),
+    minutesString = minutes.toString(),
+    correctMinutes = minutesString.padStart(2, "0");
+
+  return correctMinutes;
 };
 
 const getCorrect = (year, month, day, hours, minutes) => {
-    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
 };
-
 
 module.exports = { dateFormatter };
