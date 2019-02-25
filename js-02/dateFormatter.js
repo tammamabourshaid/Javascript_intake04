e3a708b2c3b8ad7a8b40fcf85fd2b8c05797af57
Marj-11 <marj.hajyahya@gmail.com>
2019-02-25 23:19:27 +0100

    new

--- js-02/dateFormatter.js
+++ js-02/dateFormatter.js
@@ -1,13 +1,11 @@
 function dateFormatter(date){
 
-    
-
-    var year = date.getFullYear();
-    var month = date.getMonth()+1;
-    var day = date.getDate();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
+    let year = date.getFullYear();
+    let month = date.getMonth()+1;
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
     
     if (day < 10) {
       day = '0' + day;
@@ -16,7 +14,7 @@ function dateFormatter(date){
       month = '0' + month;
     }
     
-    var formattedDate = year + '-' + month + '-' + day + " " + hours + ":" +
+    let formattedDate = year + '-' + month + '-' + day + " " + hours + ":" +
      minutes + seconds;
     return formattedDate;
 }
--- js-02/pipe.js
+++ js-02/pipe.js
@@ -1,11 +1,5 @@
 function pipe(functionArray){
 
-    let result = 0;
-
-    functionArray.forEach(element => { 
-        result = element(result);
-    });
-
-    return result;
+  
 }
 module.exports = {pipe};
