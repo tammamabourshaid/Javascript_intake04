0ae004ab01be129c6b0aa87a4a116095f24593ae
mu7m3d <mu7am2d@gmail.com>
2019-02-25 08:54:31 +0100

    calculatePolish

--- js-01/calculatePolish.js
+++ js-01/calculatePolish.js
@@ -1,5 +1,12 @@
-function calculatePolish(operator, number1, number2) {
-    return eval(number1 + operator + number2)
-}
-
+const calculatePolish = ( operator,number1, number2) => {
+    if (operator === "+") {
+        return number1 + number2;
+    } else if (operator === "*") {
+        return number1 * number2;
+    } else if (operator === "/") {
+        return number1 / number2;
+    } else if (operator === "-") {
+        return number1 - number2;
+    }
+};
 module.exports = { calculatePolish };
