4b3cd0062f36e4638800142a98c1daa89c875f35
AdibZidan <94amsz@gmail.com>
2019-02-25 06:31:54 +0100

    clean code

--- js-02/flatten.js
+++ js-02/flatten.js
@@ -1,15 +1,18 @@
 const flatten = array => {
-    return flattenArray(array);
+  return flattenArray(array);
 };
 
 const flattenArray = array => {
-    const flatten = array.reduce(
-        (accumulator, currentValue) => accumulator.concat(isArray(currentValue) ? flattenArray(currentValue) : currentValue), []);
-    return flatten;
+  const flatten = array.reduce(
+    (accumulator, currentValue) =>
+      accumulator.concat(
+        isArray(currentValue) ? flattenArray(currentValue) : currentValue),[]);
+        
+  return flatten;
 };
 
 const isArray = array => {
-    return Array.isArray(array);
+  return Array.isArray(array);
 };
 
 module.exports = { flatten };
