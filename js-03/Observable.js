adaa107a8d958cfa8477727c705d7c1369cb8233
tina-fernan <tinaaroom@gmail.com>
2019-03-03 11:19:31 +0100

    solution

--- js-03/Observable.js
+++ js-03/Observable.js
@@ -8,14 +8,17 @@ class Observable {
         this.array.push(number);
     }
     subscribe(fn) {
-        const subscribtion = [];
-        for (let index = 0; index < this.array.length; index++) {
-            const element = this.array[index];
-            const elementFunction = fn(element);
-            subscribtion.push(elementFunction);
-        }
-        this.array = [];
+        let subscribtion=this.array.map(number=>fn(number));
+        this.array=[];
         return subscribtion;
-    }
+    //     const subscribtion = [];
+    //     for (let index = 0; index < this.array.length; index++) {
+    //         const element = this.array[index];
+    //         const elementFunction = fn(element);
+    //         subscribtion.push(elementFunction);
+    //     }
+    //     this.array = [];
+    //     return subscribtion;
+     }
 }
 module.exports = { Observable };
