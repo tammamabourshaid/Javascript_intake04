f195f4c61a56a957616859347f833d4aea73f45a
janamava <aizhana.kamalova@gmail.com>
2019-03-04 14:39:32 +0100

    Solution to js-04 getPropertiesForTen

---
+++ js-04/getPropertiesForTen.js
@@ -0,0 +1,19 @@
+function getPropertiesForTen(object) {
+
+    let propsMake10 = [];
+    let keys = Object.keys(object);
+
+    keys.forEach(key => {
+        if (object[key].constructor === String) {
+            return;
+        }
+        else if (parseInt(key) + parseInt(object[key]) == 10) {
+            propsMake10.push(key);
+        }
+    });
+
+    return propsMake10;
+
+}
+
+module.exports = { getPropertiesForTen };
