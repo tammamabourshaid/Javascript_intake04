ed857c12b654fed93c65105563c2d58cc96a4ea8
Moebius05 <Paul_Friedrich@gmx.at>
2019-03-08 15:01:35 +0100

    weiter, weiter

--- js-03/BankAccount.js
+++ js-03/BankAccount.js
@@ -1,5 +1,9 @@
-function getBalance(){
-    return bankAccount.deposit;
+class BankAccount {
+    constructor(balance = 0) { this.balance = balance; }
+    getBalance() { return this.balance; }
+    deposit(amount) { this.balance = this.balance + amount; }
+    withdraw(amount) { this.balance = this.balance - amount; }
 }
 
-module.exports = { getBalance };
+
+module.exports = { BankAccount };
---
+++ js-03/Person.js
@@ -0,0 +1,50 @@
+class Person {
+    constructor(firstName, lastName, dateOfBirth) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.dateOfBirth = dateOfBirth;
+  
+      this.parents = [];
+      this.children = [];
+    }
+  
+    getFirstname() {
+      return this.firstName;
+    }
+  
+    setMother(name) {
+      return this.parents.unshift(name);
+    }
+  
+    setFather(name) {
+      return this.parents.push(name);
+    }
+  
+    getParents() {
+      return this.parents;
+    }
+  
+    addChild(name) {
+      return this.children.push(name);
+    }
+  
+    getSiblings() {
+      return this.parents[0].children.sort((child1, child2) =>
+        (child1.dateOfBirth - child2.dateOfBirth));
+    }
+  
+    getChildren() {
+      return this.children.sort((child1, child2) => {
+        return child1.dateOfBirth - child2.dateOfBirth;
+      });
+    }
+  }
+  const fritz = new Person("Fritz", "Huber");
+  const gernot = new Person("Gernot", "Huber");
+  const maria = new Person("Maria", "Huber");
+  
+  fritz.setFather(gernot);
+  fritz.setMother(maria);
+  
+  console.log(fritz)
+  module.exports = { Person };
