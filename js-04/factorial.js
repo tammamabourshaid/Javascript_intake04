193813b5c054bf5d70352b73d4a175a9413d63df
MohamedAlHammoud <mu7am2d@gmail.com>
2019-03-10 11:22:04 +0100

    factorial 2 ways of code

--- js-04/factorial.js
+++ js-04/factorial.js
@@ -7,3 +7,17 @@ module.exports = {
 
 
 //found this on google instead of loop.
+
+// function factorial(number) {
+
+//     let result = 1;
+
+//     if (number === 0) {
+//         return 1;
+//     }
+//     while (number > 0) {
+//         result = number * result;
+//         number = number - 1;
+//     }
+//     return result;
+// }
