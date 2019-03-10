19e5db283408f5538ea03783a7682768823918d0
Moebius05 <Paul_Friedrich@gmx.at>
2019-03-10 12:42:32 +0100

    weiter, weiter

---
+++ js-03/PersonParser.js
@@ -0,0 +1,27 @@
+class PersonParser {
+    constructor() { }
+  
+    parse(csv) {
+      const split = csv.split("\n"),
+        parse = split
+          .map(element => element.split(" "))
+          .filter(element => !element.includes(""))
+          .map(element => [element].toString().split(","))
+          .map(element => transformIntoPerson(element));
+  
+      return parse;
+    }
+    
+  }
+  
+  const transformIntoPerson = object => {
+    const person = {
+      firstname: object[0],
+      lastname: object[1],
+      gender: object[2]
+    };
+  
+    return person;
+  }
+  
+  module.exports = { PersonParser };
