32b9f21e7a20ec58024d2d8d5e46428edce7b3d8
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2019-03-14 12:46:46 +0100

    lesson 6 exercises

---
+++ js-06/mapCallbackToPromise.spec.js
@@ -0,0 +1,35 @@
+const { mapCallbackToPromise } = require("./mapCallbackToPromise");
+
+describe(`
+The CallBackToPromiseMapper should transform any function that
+expects as its last argument a callback to a function that
+returns a Promise instead.
+Hint: 
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
+`, () => {
+  it("should map an adder function", async () => {
+    const adder = (number1, number2, callback) => callback(number1 + number2);
+    const promiseAdder = mapCallbackToPromise(adder);
+
+    expect(await promiseAdder(1, 1)).toBe(2);
+    expect(await promiseAdder(5, 3)).toBe(8);
+  });
+
+  it("should map a function that always returns the current time", async () => {
+    const before = new Date().getTime();
+    const now = callback => callback(new Date());
+    const nowPromise = mapCallbackToPromise(now);
+
+    expect((await nowPromise()).getTime()).toBeGreaterThanOrEqual(before);
+    expect((await nowPromise()).getTime()).toBeLessThanOrEqual(
+      new Date().getTime()
+    );
+  });
+
+  it("should map a string converter", async () => {
+    const toString = (number, callback) => callback(number + "");
+    const toStringPromise = mapCallbackToPromise(toString);
+
+    expect(await toStringPromise(10)).toBe("10");
+  });
+});
