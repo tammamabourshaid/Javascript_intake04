e8aa4bc4ce68811cdd6f1f1977a821dd93ea0f83
Moebius05 <Paul_Friedrich@gmx.at>
2019-03-20 15:43:31 +0100

    ok

--- js-01/median.spec.js
+++ js-01/median.spec.js
@@ -9,11 +9,8 @@ describe("getMedian", () => {
 
   it("should get median of an odd list", () =>
     expect(getMedian([2, 3, 4, 5])).toBe(3.5));
-<<<<<<< HEAD
-=======
 
   it("should ensure that the sort is working", () => {
     expect(getMedian([11, 1, 2, 3, 4])).toBe(3);
   });
->>>>>>> ea0b447967b7bc353f7c2a3243c1d29e174f594e
 });
