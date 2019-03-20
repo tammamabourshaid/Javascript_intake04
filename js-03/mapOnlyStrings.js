c05c3ca6b80a50b80a13b17d0451c26788c2d5f4
Moebius05 <Paul_Friedrich@gmx.at>
2019-03-20 23:33:49 +0100

    fertig

---
+++ js-03/mapOnlyStrings.js
@@ -0,0 +1,17 @@
+const mapOnlyStrings = (array, condition) => {
+    const transformed = [];
+  
+    array.forEach(element => {
+      const mapped = condition(element);
+  
+      if (typeof mapped !== "string") {
+        transformed.push(element)
+      } else {
+        transformed.push(mapped)
+      }
+    });
+  
+    return transformed;
+  };
+  
+  module.exports = { mapOnlyStrings };
