a1cd0c75ad59d9887db4a84febc6a6f195bd33fc
janamava <aizhana.kamalova@gmail.com>
2019-03-24 21:28:15 +0100

    Implemented user story 1 and 2

---
+++ chess/dictionary.js
@@ -0,0 +1,26 @@
+dictionary = {
+
+    row: {
+        1: 8,
+        2: 7,
+        3: 6,
+        4: 5,
+        5: 4,
+        6: 3,
+        7: 2,
+        8: 1
+    },
+
+    column: {
+        a: 0,
+        b: 1,
+        c: 2,
+        d: 3,
+        e: 4,
+        f: 5,
+        g: 6,
+        h: 7
+    }
+}
+
+module.exports = { dictionary }
