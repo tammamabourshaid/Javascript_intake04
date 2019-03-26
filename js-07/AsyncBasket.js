f86b126cf5c22660c5dbeef415f4c121892927cf
AdibZidan <94amsz@gmail.com>
2019-03-26 15:53:49 +0100

    solved week 7

---
+++ js-07/AsyncBasket.js
@@ -0,0 +1,15 @@
+class AsyncBasket {
+  constructor() {
+    this.products = [];
+  }
+
+  put(product) {
+    return new Promise(resolve => setTimeout(() => resolve(this.products.push(product)), 500));
+  }
+
+  get() {
+    return new Promise(resolve => setTimeout(() => resolve(this.products), 500));
+  }
+}
+
+module.exports = { AsyncBasket };
