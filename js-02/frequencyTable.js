cad33251ca28d9f540af6ad0d3e76e1174766781
janamava <aizhana.kamalova@gmail.com>
2019-03-27 15:25:17 +0100

    Solution to js-02 frequencyTable

--- js-02/frequencyTable.js
+++ js-02/frequencyTable.js
@@ -1,8 +1,19 @@
-function getFrequencyTable(array){
-
-
+const getFrequencyTable = function (array) {
     
+    let object = {};
+    let value = 1;
 
+    array.forEach(element => {
 
+        if (!object.hasOwnProperty(element)) {
+            object[element] = value;
+        }
 
+        else {
+            object[element] += 1;
+        }
+    });
+    return object;
 }
+
+module.exports = { getFrequencyTable };
