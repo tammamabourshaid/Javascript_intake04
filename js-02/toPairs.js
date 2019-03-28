137f54695f7562a5a174e111576a3a3217c676a2
badisalim <badisalim@gmail.com>
2019-03-28 15:04:16 +0100

    update

--- js-02/toPairs.js
+++ js-02/toPairs.js
@@ -1,20 +1,31 @@
- function toPairs(list) {
-const result= Object.entries(list).sort((a, b) => b[1] - a[1]);
-console.log(result);
+//  function toPairs(list) {
+//    const result = Object.entries(list).sort((a, b) => b[1] - a[1]);
+//    console.log(result);
+//    return result;
+//  }
+//  //     return [key,value];
+//  // }
 
-return result;
+//  module.exports = {
+//    toPairs
+//  };
+//  //---------------------------
+//  //other solution
+//  function toPairs(object) {
+//    return Object.keys(object)
+//      .sort()
+//      .map(key => [key, object[key]]);
+//  }
+//  module.exports = {
+//    toPairs
+//  };
+//  //----------------------------------
+ //lodash solution
+ const _ = require("lodash");
+
+ function toPairs(object) {
+   return _.toPairs(object).sort();//sort((a,b)=>b[1] - a[1]);
  }
-//     return [key,value];
-// }
-
-
-module.exports = { toPairs };
-
-//other solution
-function toPairs(object) {
-    return Object.keys(object)
-      .sort()
-      .map(key => [key, object[key]]);
-  }
-  
-  module.exports = { toPairs };
+ module.exports = {
+  toPairs
+};
