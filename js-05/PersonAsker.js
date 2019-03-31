a24cd95b0e230f032f4c317ab49edc3b29d75966
tabourshaid <tabourshaid@users.noreply.github.com>
2019-03-31 18:16:53 +0200

    js-05 "solution"

---
+++ js-05/PersonAsker.js
@@ -0,0 +1,23 @@
+class PersonAsker {
+
+    constructor(readLineMock) {
+        this.readLineMock = readLineMock;
+        this.name = "";
+        this.country = "";
+    }
+
+    ask() {
+        const fullName = this.readLineMock("What is your product?");
+        fullName.then(e => this.name += e);
+
+        const country = this.readLineMock("Where do you come from?");
+        country.then(e => this.country += e);
+    }
+
+    getInfo() {
+        return this.name + " is from " + this.country;
+
+    }
+}
+
+module.exports = { PersonAsker };
