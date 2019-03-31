4cd119810d0704ee369c36b24424ef089bfcc93f
janamava <aizhana.kamalova@gmail.com>
2019-03-31 14:25:59 +0200

    Solution to js-02 getProperty

--- js-02/getProperty.js
+++ js-02/getProperty.js
@@ -1,5 +1,22 @@
-function getProperty(parameterOne, parameterTwo){
-    return parameterOne["" + parameterTwo];
+function getProperty(obj, string) {
+
+    let keys = string.split(".");
+    let object = obj;
+    let key = keys.shift();
+
+    if (!object[key]) {
+        return null;
+    }
+    while (typeof object[key] === "object") {
+        object = object[key];
+        key = Object.getOwnPropertyNames(object);
+
+        if (!object[key]) {
+            return null;
+        }
+    }
+
+    return object[key];
 }
 
-module.exports = {getProperty};
+module.exports = { getProperty };
