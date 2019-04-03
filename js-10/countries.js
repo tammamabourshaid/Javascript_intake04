f5a7e5e30c483c647a8b3709c56b79ac952f45f8
AdibZidan <94amsz@gmail.com>
2019-04-03 12:15:42 +0200

    solved HTTP request and JS-10

---
+++ js-10/countries.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+request('https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population-density.json', (error, response, body) => {
+  // console.log('error:', error);
+  // console.log('statusCode:', response && response.statusCode);
+
+  const countries = JSON.parse(body);
+
+  const top3MostDenseCountries = countries
+    .sort((country1, country2) => country2.density - country1.density)
+    .splice(0, 3);
+  console.log('Most dense countries:');
+  console.log(top3MostDenseCountries);
+
+  console.log('------');
+
+  const top3LeastDenseCountries = countries
+    .sort((country1, country2) => country1.density - country2.density)
+    .filter(country => country.density !== null && country.density !== '0')
+    .splice(0, 3);
+
+  console.log('Least dense countries:');
+  console.log(top3LeastDenseCountries);
+});
