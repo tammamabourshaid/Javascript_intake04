02c647b1d4b0b8159fb83a63cc22027ad92d2639
tabourshaid <tabourshaid@users.noreply.github.com>
2019-04-07 22:39:41 +0200

    js-01 "solution"

--- js-01/average.js
+++ js-01/average.js
@@ -6,3 +6,7 @@ function getAverage(array) {
 getAverage([5, 10, 11, 1, 3]);
 
 module.exports = { getAverage };
+
+// The reduce() method reduces the array to a single value.
+// The reduce() method executes a provided function for each value of the array (from left-to-right).
+// The return value of the function is stored in an accumulator (result/total).
