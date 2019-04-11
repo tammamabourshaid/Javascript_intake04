cf6a8476d8e6e506b45b06bbde2295fa062b3a92
AdibZidan <94amsz@gmail.com>
2019-04-11 17:25:36 +0200

    solved ChainableCalculator

---
+++ js-14/ChainableCalculator.js
@@ -0,0 +1,32 @@
+class ChainableCalculator {
+  constructor(number) {
+    this.number = number;
+  }
+
+  add(number) {
+    this.number += number;
+    return this;
+  }
+
+  subtract(number) {
+    this.number -= number;
+    return this;
+  }
+
+  multiply(number) {
+    this.number *= number;
+    return this;
+  }
+
+  divide(number) {
+    this.number /= number;
+    return this;
+  }
+
+  getResult() {
+    return this.number;
+  }
+
+}
+
+module.exports = { ChainableCalculator };
