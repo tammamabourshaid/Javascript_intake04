eed188a740edf1d983980f9b0518858e9dc9af31
tabourshaid <tabourshaid@users.noreply.github.com>
2019-04-12 14:37:46 +0200

    js-02 "solution"

--- js-02/getProperty.spec.js
+++ js-02/getProperty.spec.js
@@ -1,17 +1,17 @@
 const { getProperty } = require("./getProperty");
 
 describe("getProperty", () => {
-  it("should return the property of an object", () => {
+  it("should return the properties of an object", () => {
     const country = { name: "Austria" };
     expect(getProperty(country, "name")).toBe("Austria");
   });
 
-  it("should return the nested property of an object", () => {
+  it("should return the nested properties of an object", () => {
     const car = { name: "Vectra", engine: { fuel: "Diesel" } };
     expect(getProperty(car, "engine.fuel")).toBe("Diesel");
   });
 
-  it("should return null even for number2 non-existing property", () => {
+  it("should return null even for number2 non-existing properties", () => {
     const person = { firstname: "Friedrich", lastname: "Schiller" };
     expect(getProperty(person, "birthplace.grossPrice")).toBeNull();
   });
