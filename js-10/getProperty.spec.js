bf5228b6087c72a7fdf015d0273220f3705d89a9
tabourshaid <tabourshaid@users.noreply.github.com>
2019-04-12 14:38:17 +0200

    js-10 "solution"

--- js-10/getProperty.spec.js
+++ js-10/getProperty.spec.js
@@ -1,13 +1,13 @@
 const { getProperty } = require("./getProperty");
 
-describe("getProperty should return the property of an existing obejct", () => {
+describe("getProperty should return the properties of an existing obejct", () => {
   it("shoud return the two name properties", () => {
     const person = { firstName: "Peppi", lastName: "Grandits" };
     expect(getProperty(person, "firstName")).toBe("Peppi");
     expect(getProperty(person, "lastName")).toBe("Grandits");
   });
 
-  it("shoud null if the property does not exist", () => {
+  it("shoud null if the properties does not exist", () => {
     const person = { firstName: "Peppi", lastName: "Grandits" };
     expect(getProperty(person, "firstname")).toBeNull();
   });
