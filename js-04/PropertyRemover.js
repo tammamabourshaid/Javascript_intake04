f6340c7f0b22ae84d40a16237502c793a6f51b75
AdibZidan <94amsz@gmail.com>
2019-04-16 22:01:34 +0200

    solve PropertyRemover.js

--- js-04/PropertyRemover.js
+++ js-04/PropertyRemover.js
@@ -1,14 +1,31 @@
+const { deleteProperty } = require('../js-04/deleteProperty.js');
+
 class PropertyRemover {
   constructor() {
-
+    this.array = []; this.object; this.property;
   }
 
-  addPropertyToBeRemoved(object, property) {
-    return delete object[property];
+  addPropertyToBeRemoved(property) {
+    if (Array.isArray(property)) {
+      this.array = property;
+    }
+    const propertyToRemove = this.property = property;
+    this.array.push(propertyToRemove);
   }
 
-  process() {
-    return;
+  process(object) {
+    const values = Object.values(object),
+      original = this.object = object,
+      properties = this.array,
+      copyOfObject = Object.assign({}, original);
+
+    properties.forEach(element => deleteProperty(copyOfObject, element))
+
+    if (properties.toString() === values.toString()) {
+      throw Error();
+    }
+
+    return copyOfObject;
   }
 }
 
