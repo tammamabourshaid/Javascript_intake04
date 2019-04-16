4000cad67c6f4e563b37ab68a01c05d114a0321d
AdibZidan <94amsz@gmail.com>
2019-04-16 22:25:05 +0200

    solved objectToArray.js

--- js-04/objectToArray.js
+++ js-04/objectToArray.js
@@ -1,13 +1,13 @@
 const objectToArray = object => {
-  const array = [];
-  const keys = Object.keys(object);
+  const array = [],
+    keys = Object.keys(object);
 
   keys.forEach(property => {
     const value = object[property];
 
     if (typeof value === "object") {
-      keys.forEach(arrayOfNestedObject =>
-        array.push([value, ...arrayOfNestedObject]));
+      objectToArray(value).map(arrayOfNestedObject =>
+        array.push([property, ...arrayOfNestedObject]));
     } else {
       array.push([property, value]);
     }
