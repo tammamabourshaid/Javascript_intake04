e63399ef5f1681ccee4c2d0265b124055e1d2d09
tabourshaid <tabourshaid@users.noreply.github.com>
2019-04-26 10:35:19 +0200

    web-16

--- web-16/01-exercise.spec.js
+++ web-16/01-exercise.spec.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
-const html = fs.readFileSync("web-16/01-exercise.html", "utf8");
+const html = fs.readFileSync("./01-exercise.html", "utf8");
 
 jest.dontMock("fs");
 
 describe("web exercise 1", () => {
-  it("should check if we add an img and p tag", () => {
-    expect(document.getElementsByTagName("img")).toHaveLength(0);
-    expect(document.getElementsByTagName("p")).toHaveLength(0);
-    require("./01-exercise.js");
-    expect(document.getElementsByTagName("img")).toHaveLength(1);
-    expect(document.getElementsByTagName("p")).toHaveLength(1);
-  });
+    it("should check if we add an img and p tag", () => {
+        expect(document.getElementsByTagName("img")).toHaveLength(0);
+        expect(document.getElementsByTagName("p")).toHaveLength(0);
+        require("./01-exercise.js");
+        expect(document.getElementsByTagName("img")).toHaveLength(1);
+        expect(document.getElementsByTagName("p")).toHaveLength(1);
+    });
 });
---
+++ web-16/03-exercise.js
@@ -0,0 +1,4 @@
+const element1 = document.getElementsByTagName("img")[1];
+const element2 = document.getElementsByTagName("img")[0];
+
+element1.after(element2);
