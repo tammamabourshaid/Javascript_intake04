bf831a2af3558e2efb6e09ac09675b8c87531f2a
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2019-04-29 12:38:18 +0200

    source code of lesson 17

---
+++ web-17/01-exercise.spec.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const html = fs.readFileSync("./web-17/01-exercise.html", "utf8");
+
+jest.dontMock("fs");
+
+describe("web exercise 1", () => {
+  it("should add Austrian rivers", () => {
+    document.documentElement.innerHTML = html.toString();
+    expect(document.getElementsByTagName("ul")).toHaveLength(0);
+    require("./01-exercise.js");
+
+    const [ul] = document.getElementsByTagName("ul");
+    const riverListItems = ul.children;
+    const rivers = [
+      "Danube",
+      "Inn",
+      "Mur",
+      "Raab",
+      "Rhein",
+      "Salzach",
+      "Drau",
+      "Enns"
+    ];
+
+    rivers.forEach((river, ix) =>
+      expect(riverListItems[ix].innerHTML).toBe(river)
+    );
+  });
+});
