4a6859b2d46b2ad9a0815bbe496b6943e497932a
AdibZidan <94amsz@gmail.com>
2019-04-29 13:03:49 +0200

    work in progress

--- web-16/05-exercise.js
+++ web-16/05-exercise.js
@@ -1,3 +1,9 @@
 const paragraph = document.getElementsByTagName('p');
+const footer = document.getElementsByTagName('footer');
+const heading = document.getElementsByTagName('h1');
 
-window.replace = (text, anotherText) => text = paragraph[0].innerHTML = anotherText;
+window.replace = (text, anotherText) => {
+  text = heading[0].innerHTML = anotherText;
+  text = footer[0].innerHTML = anotherText;
+  text = paragraph[0].innerHTML = anotherText;
+};
