11151d4c36bfba77b5d288a17f5ace02ea254e2b
janamava <aizhana.kamalova@gmail.com>
2019-04-30 14:11:18 +0200

    Inserts h1 before first p element

--- web-17/02-exercise.js
+++ web-17/02-exercise.js
@@ -1,5 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
+
+    const pTags = document.getElementsByTagName("p");
     const headerTag = document.createElement("h1");
-    document.body.appendChild(headerTag);
+    document.body.insertBefore(headerTag, pTags[0])
     headerTag.innerHTML = Array.from(document.body.children).filter(element => element.localName !== "script").length;
 });
+
+
+
