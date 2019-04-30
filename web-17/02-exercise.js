8f93db3c72b7f00fbbcea70c4aac2114203a52a4
AdibZidan <94amsz@gmail.com>
2019-04-30 14:11:47 +0200

    solved web-17/02-exercise.js correctly

--- web-17/02-exercise.js
+++ web-17/02-exercise.js
@@ -1,3 +1,4 @@
+// Hacked Version
 // const outerHeading = document.createElement('h1');
 // outerHeading.innerHTML = 4;
 // document.body.append(outerHeading)
@@ -6,11 +7,13 @@
 //   const innerHeading = document.createElement('h1');
 //   innerHeading.innerHTML = 5;
 //   document.body.append(innerHeading);
+//   document.body.insertBefore(innerHeading, outerHeading);
 // });
 
 window.addEventListener("DOMContentLoaded", () => {
   const headingOne = document.createElement("h1");
-  document.body.appendChild(headingOne);
+  const firstParagraph = document.getElementsByTagName('p')[0];
+  document.body.insertBefore(headingOne, firstParagraph);
   headingOne.innerHTML = Array
     .from(document.body.children)
     .filter(tag => tag.localName !== "script")
