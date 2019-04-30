ff3a5d44d582c33faabc4acc49abb55c396e429e
AdibZidan <94amsz@gmail.com>
2019-04-30 10:00:39 +0200

    solved web-17/04-exercise.js and all the exercises

---
+++ web-17/04-exercise.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const [form] = document.getElementsByTagName('form');
+  const [firstName, lastName] = document.getElementsByTagName('input');
+  const [paragraph] = document.getElementsByTagName('p');
+
+  form.addEventListener('submit', () => {
+    paragraph.innerHTML = `${firstName.value} ${lastName.value}`;
+  });
+})
