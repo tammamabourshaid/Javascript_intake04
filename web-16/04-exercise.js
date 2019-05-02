85b62c6e78dfe9d86474a3a14c5e7cf14253fb1c
AdibZidan <94amsz@gmail.com>
2019-05-02 11:25:15 +0200

    work in progress

--- web-16/04-exercise.js
+++ web-16/04-exercise.js
@@ -1,28 +1,43 @@
 window.render = DOMReplica => {
   const tag = document.createElement(DOMReplica.tag);
-  const children = DOMReplica.children;
   tag.className = DOMReplica.className;
 
-  children.forEach(child => {
-    if (child.className) {
-      const innerChildren = child.children;
-      const childTag = document.createElement(child.tag);
-      const className = child.className;
-      childTag.className = className;
-      tag.appendChild(childTag)
+  if (!DOMReplica.children) {
+    const text = DOMReplica.text;
+    tag.innerHTML = text;
+  } else {
+    const children = DOMReplica.children;
+    children.forEach(child => {
+      if (child.className) {
+        const innerChildren = child.children;
+        const childTag = document.createElement(child.tag);
+        const className = child.className;
+        childTag.className = className;
+        tag.appendChild(childTag);
 
-      innerChildren.forEach((innerChild) => {
-        const innerChildTag = document.createElement(innerChild.tag);
-        childTag.appendChild(innerChildTag)
-        innerChildTag.className = innerChild.className;
-        innerChildTag.innerHTML = innerChild.text;
-      });
-    } else {
-      const childTag = document.createElement(child.tag);
-      childTag.src = child.src;
-
-      tag.appendChild(childTag);
-    }
-  });
+        innerChildren.forEach((innerChild) => {
+          const innerChildTag = document.createElement(innerChild.tag);
+          childTag.appendChild(innerChildTag);
+          innerChildTag.className = innerChild.className;
+          innerChildTag.innerHTML = innerChild.text;
+        });
+      } else {
+        const childTag = document.createElement(child.tag);
+        childTag.src = child.src;
+        tag.appendChild(childTag);
+      }
+    });
+  }
   document.body.appendChild(tag);
 };
+
+window.render({
+  text: "This is an image",
+  children: [
+    {
+      tag: "img",
+      src: "Drau.jpg",
+      alt: "Drau"
+    }
+  ]
+})
