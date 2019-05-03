e3d84125884243cdbaa8da7edac5b088a727862f
AdibZidan <94amsz@gmail.com>
2019-05-03 08:49:05 +0200

    solve web-18/basic/directions.ts

---
+++ web-18/basic/directions.ts
@@ -0,0 +1,46 @@
+export const shortenDirections = (directions: Array<string>) => {
+  let horizontal = 0,
+    vertical = 0,
+    size = directions.length;
+
+  for (let times = 0; times < size; times++) {
+    const direction = directions[times];
+    switch (direction) {
+      case "EAST":
+        horizontal++;
+        break;
+      case "WEST":
+        horizontal--;
+        break;
+      case "NORTH":
+        vertical++;
+        break;
+      case "SOUTH":
+        vertical--;
+        break;
+    }
+  }
+
+  const correctDirections: Array<string> = [];
+
+  const putCorrectDirections = (direction: string, times: number) => {
+    for (let position = 0; position < times; position++) {
+      correctDirections.push(direction);
+    }
+  };
+
+  if (horizontal > 0) {
+    putCorrectDirections("EAST", horizontal);
+  }
+  if (horizontal < 0) {
+    putCorrectDirections("WEST", horizontal * -1);
+  }
+  if (vertical > 0) {
+    putCorrectDirections("NORTH", vertical);
+  }
+  if (vertical < 0) {
+    putCorrectDirections("SOUTH", vertical * -1);
+  }
+
+  return correctDirections;
+};
