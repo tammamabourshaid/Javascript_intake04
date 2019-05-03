46caaafbae32f1c2a80df6673cc09a7fe66d42b1
tabourshaid <tabourshaid@users.noreply.github.com>
2019-05-03 08:07:28 +0200

    web-16

--- web-16/03-exercise.spec.js
+++ web-16/03-exercise.spec.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
-const html = fs.readFileSync("./03-exercise.html", "utf8");
+const html = fs.readFileSync("./web-16/03-exercise.html", "utf8");
 
 jest.dontMock("fs");
 
 describe("web exercise 3", () => {
-    it("should swap the positions of the two images", () => {
-        document.documentElement.innerHTML = html.toString();
-        let [drau, angkor] = document.getElementsByTagName("img");
-        expect(drau.src).toMatch(/Drau/);
-        expect(angkor.src).toMatch(/Angkor/);
-        require("./03-exercise.js");
-        [angkor, drau] = document.getElementsByTagName("img");
-        expect(drau.src).toMatch(/Drau/);
-        expect(angkor.src).toMatch(/Angkor/);
-    });
+  it("should swap the positions of the two images", () => {
+    document.documentElement.innerHTML = html.toString();
+    let [drau, angkor] = document.getElementsByTagName("img");
+    expect(drau.src).toMatch(/Drau/);
+    expect(angkor.src).toMatch(/Angkor/);
+    require("./03-exercise.js");
+    [angkor, drau] = document.getElementsByTagName("img");
+    expect(drau.src).toMatch(/Drau/);
+    expect(angkor.src).toMatch(/Angkor/);
+  });
 });
